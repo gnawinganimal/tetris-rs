59d3a1c1a75beeb58d95cc91c6922f86133b6a31
gnawinganimal <unrongeur@proton.me>
2023-01-12 15:14:37 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::{io, thread, time::Duration};
+use crossterm::{terminal::{disable_raw_mode, LeaveAlternateScreen}, execute, event::DisableMouseCapture};
+use tui::{backend::CrosstermBackend, Terminal, widgets::{Block, Borders}};
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Tetris")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
